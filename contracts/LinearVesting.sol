21c5e825548a2f27b3b2f99278f94898026e79ac
Ilia Muravev <degtyarev327@gmail.com>
2022-01-27 16:38:57 +0300

    rename linear vesting contract

---
+++ contracts/LinearVesting.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.8.0;
+
+// SPDX-License-Identifier: Unlicensed
+
+import './IVesting.sol';
+
+contract LinearVesting is IVesting {
+    uint immutable public cliffDuration;
+    uint immutable public releaseDuration;
+
+    constructor(uint _cliffDurationDays, uint _releaseDurationDays) {
+        cliffDuration = _cliffDurationDays * 1 days;
+        releaseDuration = _releaseDurationDays * 1 days;
+    }
+
+    function calcVestedAmount(uint _startTime, uint _allowance) external view override returns(uint) {
+        uint vestedAmount = 0;
+        uint timedelta = block.timestamp - _startTime;
+
+        if (timedelta >= cliffDuration) {
+            if (timedelta >= cliffDuration + releaseDuration) {
+                vestedAmount = _allowance;
+            } else {
+                vestedAmount = (timedelta - cliffDuration) * _allowance / releaseDuration;
+            }
+        }
+
+        return vestedAmount;
+    }
+}
