ecb6da75cc193287cbb1c28c819119bd32159074
Ilia Muravev <degtyarev327@gmail.com>
2022-01-28 16:17:19 +0300

    rename contracts

--- contracts/IVestingStrategy.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.8.0;
-
-// SPDX-License-Identifier: Unlicensed
-
-interface IVestingStrategy {
-    function calcVestedAmount(uint _startTime, uint _allowance) external view returns(uint);
-}
