5ccd320653d624730178ded0ef91a0a127cef984
Ilia Muravev <degtyarev327@gmail.com>
2022-02-20 15:22:37 +0300

    improve security and add ethrescan contract verification plugin for hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,23 +7,23 @@ require('./tasks/deploy-and-init.js');
 require('./tasks/time.js');
 require('@nomiclabs/hardhat-etherscan');
 
-const {secret} = require('./secret.js');
+const {signers, apiKeys, providers} = require('./secret.js');
 
 module.exports = {
     solidity: "0.8.4",
     networks: {
         'local': {
-            'url': 'http://127.0.0.1:8545',
-            'accounts': [secret]
+            'url': providers.local,
+            'accounts': signers.local
         },
         'ropsten': {
-            'url': 'https://ropsten.infura.io/v3/e5bc8d39e7f14ab29c3af815011e15a1',
-            'accounts': [secret]
+            'url': providers.ropsten,
+            'accounts': signers.ropsten
         }
     },
     etherscan: {
         apiKey: {
-            'ropsten': "https://api-ropsten.etherscan.io"
+            'ropsten': apiKeys.ropsten
         }
     }
 };
